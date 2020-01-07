5e7bcd6c37639b7ca7c05ff1d339e549062420e5
sabya48 <57270607+sabya48@users.noreply.github.com>
2020-01-08 02:13:32 +0530

    initial commit

---
+++ calculate area and perimter of circle.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	double radius,area,perimeter;
+	cout<<"enter the radius of the circle"<<endl;
+	cin>>radius;
+	
+	perimeter=2*3.14159*radius;
+	area=3.14159*radius*radius;
+	cout<<"the area of the circle: "<<area<<endl;
+	cout<<"the perimeter of the circle: "<<perimeter<<endl;
+	
+	return 0;
+}
